47f90c55787ac53877fb72a358a35492d696bd81
Mohamed Nheri <nherimohamed@gmail.com>
2019-10-29 02:37:28 -0400

    Initial commit

---
+++ src/main/java/edu/umgc/cmsc495/ene/EneApplication.java
@@ -0,0 +1,13 @@
+package edu.umgc.cmsc495.ene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/umgc/cmsc495/ene/EneApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.umgc.cmsc495.ene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
