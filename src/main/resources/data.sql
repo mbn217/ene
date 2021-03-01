521d6095d8283efa55b458810409851cc86c9170
Mohamed Nheri <24482547+mbn217@users.noreply.github.com>
2021-03-01 10:54:11 -0500

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO application VALUES ('NDA','200200');
-INSERT INTO application VALUES ('ANDA','77772');
+INSERT INTO users VALUES (1,'test','test','test@email.com');
+INSERT INTO users VALUES (2,'admin','admin','admin@email.com');
 
-INSERT INTO generalInfo VALUES ('Rev-biometrics-21','N/A','Nothing');
-INSERT INTO generalInfo VALUES ('rev-safetyIIS-06','N/A','Nothing');
-
+INSERT INTO book VALUES (1,'Aladdin','XYZ','Comic','K2G3R7', CURRENT_DATE(), 200, 1);
+INSERT INTO book VALUES (2,'Harry Potter','J.K Rowling','Comic','123CBC', CURRENT_DATE(), 340, 1);
+INSERT INTO book VALUES (3,'Harry Potter 2','J.K Rowling','Comic','456BCD', CURRENT_DATE(), 350, 1);
 
