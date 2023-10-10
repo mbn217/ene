f5e72b92369f7d219d2c13522ad86649633b12f5
Mohamed Nheri <nherimohamed@gmail.com>
2023-10-09 22:36:03 -0400

    scope change

--- src/main/java/edu/umgc/cmsc495/ene/EneMain.java
+++ src/main/java/edu/umgc/cmsc495/ene/EneMain.java
@@ -2,9 +2,10 @@ package edu.umgc.cmsc495.ene;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EneMain {
+public class EneMain extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EneMain.class, args);
